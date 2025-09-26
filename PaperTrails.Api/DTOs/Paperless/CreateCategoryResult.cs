271aebc340d30f76c5c8a75415eec9544410084c
llsaimur <rashidsaimur@icloud.com>
2025-09-26 14:06:41 -0400

    Added paperless service, updated categories table column document_type_id from string to int

---
+++ PaperTrails.Api/DTOs/Paperless/CreateCategoryResult.cs
@@ -0,0 +1,9 @@
+﻿namespace PaperTrails.Api.DTOs.Paperless
+{
+    public class CreateCategoryResult
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Error { get; set; }
+    }
+}
